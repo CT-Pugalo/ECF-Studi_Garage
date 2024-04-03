8f035d45d889817a888981b57ead998e34fbebb9
CT-Pugalo <hugobogdanovic@free.fr>
2024-04-03 11:53:04 +0200

    DossierProjet

--- Préliminaire/ScriptCreation.sql
+++ Préliminaire/ScriptCreation.sql
@@ -1,1 +1,14 @@
+--Ajouter un utilisateur:
 
+CREATE USER adminBase IDENTIFIED BY 'V3ryS3cur3edP@ssw0rd';
+
+--Création de la base de donnée:
+
+CREATE DATABASE base_garage;
+
+--Ajout des droits pour l'admin:
+
+GRANT SELECT, UPDATE, DELETE, INSERT ON base_garage.* TO adminBase;
+
+
+CREATE TABLE 
